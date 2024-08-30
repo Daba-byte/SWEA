99440c060bc7c0de172c214a5c4edc48bc5dc86f
dabeen <dabin12648@gmail.com>
2024-08-30 21:02:29 +0900

    0830

--- IM대비/12712_파리퇴치3.py
+++ IM대비/12712_파리퇴치3.py
@@ -58,3 +58,63 @@ for tc in range(1, T + 1):
 
     result = max_flies(N, M, arr)
     print(f'#{tc} {result}')
+
+##### 또 풀어보자 #####
+def plus_spray(N, M, grid, x, y):
+    total_flies = 0
+    # 방향: 하, 상, 우, 좌
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    
+    total_flies += grid[x][y] # 가운데 파리다
+
+    # 각 방향으로 M-1칸만큼 스프레이
+    for dx, dy in directions:
+        for i in range(1, M):
+            nx, ny = x + dx * i, y + dy * i
+            if 0 <= nx < N and 0 <= ny < N:
+                total_flies += grid[nx][ny]
+            else:
+                break
+    return total_flies
+
+def x_spray(N, M, grid, x, y):
+    total_flies = 0
+    # 방향: 하우, 하좌, 상우, 상좌
+    directions = [(-1, -1), (1, 1), (-1, 1), (1, -1)]
+
+    total_flies += grid[x][y] # 나 가운데야 또 와써
+
+    # 대각선 방향으로 M-1칸만큼 스프레이 뿌릴 거야
+    for dx, dy in directions:
+        for i in range(1, M):
+            nx, ny = x + dx * i, y + dy * i
+            if 0 <= nx < N and 0 <= ny < N:
+                total_flies += grid[nx][ny]
+            else:
+                break
+
+    return total_flies
+
+# 파리 넌 진짜 큰일났다
+def kill_flies(N, M, grid):
+    max_kill_flies = 0
+
+    for x in range(N):
+        for y in range(N):
+            # +에서
+            flies_plus = plus_spray(N, M, grid, x, y)
+            max_kill_flies = max(max_kill_flies, flies_plus)
+            
+            # x에서
+            flies_x = x_spray(N, M, grid, x, y)
+            max_kill_flies = max(max_kill_flies, flies_x)
+    
+    return max_kill_flies
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    grid = [list(map(int, input().split())) for _ in range(N)]
+
+    result = kill_flies(N, M, grid)
+    print(F'#{tc} {result}')
--- IM대비/20397_돌뒤집기게임2.py
+++ IM대비/20397_돌뒤집기게임2.py
@@ -25,3 +25,34 @@ for tc in range(1, T + 1):
 
     # 결과 출력
     print(f'#{tc} {" ".join(map(str, stones))}')
+
+##### 함수를 쓰자  ######
+def turn_stones(N, stones, i, j):
+    center = i - 1 # 안녕 나는 중심돌이야
+        
+    # 좌우로 j개의 돌에 대해 처리
+    for offset in range(1, j + 1):
+        left = center - offset
+        right = center + offset
+            
+        # 범위를 벗어나면 중지
+        if left < 0 or right >= N:
+            break
+            
+        # left와 right의 돌이 같은 색이면 뒤집음
+        if stones[left] == stones[right]:
+            stones[left] = 1 - stones[left]
+            stones[right] = 1 - stones[right]
+        
+
+    return " ".join(map(str, stones))
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    stones = list(map(int, input().split()))
+    for _ in range(M):
+        i, j = map(int, input().split())
+
+        result = turn_stones(N, stones, i, j)
+    print(f'#{tc} {result}')
