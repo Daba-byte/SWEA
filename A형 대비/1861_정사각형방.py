eb6f28738ae4380225e4e3cc9245bc7670984dd9
daba <dabin12648@gmail.com>
2024-09-09 09:04:39 +0900

    0909

--- A형 대비/1861_정사각형방.py
+++ A형 대비/1861_정사각형방.py
@@ -1,10 +1,30 @@
+def find_room(N, room_numbers):
+    dx = [1, 0, -1, 0]
+    dy = [0, 1, 0, -1]
+
+    for i in range(N):
+        for j in range(N):
+            for K in range(4):
+                nx, ny = i + dx[k], j + dy[k]
+
+                if ny < 0 or ny >= N or nx < 0 or nx >= N:
+                    continue
+
+                if arr[ny][nx] == arr[i][j] + 1:
+                    visited[arr[i][j]] = 1
+                    break
+
+    cnt = max_cnt = start = 0
+    # 어우 졸라 어렵네
+    
+
 
 
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
-    room_numbers = []
-    for _ in range(N):
-        Ai, j = map(int, input().split())
-        room_numbers.append()
+    room_numbers = [list(map(int, input().split()))]
+
+    result = find_room(N, room_numbers)
+    print(f'#{tc} {result}')
